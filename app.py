0851ce677324d552c11c85de857d5be3ab1ab849
CamelGamer123 <kylepannan@gmail.com>
2024-03-07 12:35:49 +1000

    Updated app.py

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
-from flask import Flask
+"""
+App file for filehost project. 
+"""
 
-app = Flask(__name__)
+# Third Party Imports
+from flask import Flask, render_template as renderTemplate, Blueprint
+
+# Local Imports
+from internals.logging import createLogger
+
+# Create App Logger
+logger = createLogger(__name__)
+
+# Create Flask app
+app: Flask = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route("/")
+def _index() -> str:
+    """
+    Index route.
+    
+    Returns:
+        str: Index route message.
+    """
+    return renderTemplate("index.html")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
