13157045f71df71268b38337547dbd83113f4ff6
CamelGamer123 <kylepannan@gmail.com>
2024-03-11 12:21:10 +1000

    Current implimentation of 2fa and all current code

--- app.py
+++ app.py
@@ -17,7 +17,8 @@ from internals.models.user import User
 
 # Create App Logger
 logger: SuppressedLoggerAdapter = createLogger("App")
-endpointLogger: EndpointLoggerAdapter = createLogger("endpoints", adapterMode="endpoint", formatString="[%(asctime)s] [Endpoints] [%(levelname)s] %(message)s")  # Used for security log
+endpointLogger: EndpointLoggerAdapter = createLogger("endpoints", adapterMode="endpoint",
+                                                     formatString="[%(asctime)s] [Endpoints] [%(levelname)s] %(message)s")  # Used for security log
 
 # Create Config
 config: Config = Config()
@@ -69,7 +70,7 @@ def _cookieCheck(data: SessionMixin) -> str:
     return "Valid"
 
 
-def _verifyForm(form: dict, fields: list) -> tuple[bool, str]:
+def _verifyForm(form: dict, fields: list) -> tuple[bool, str, int]:
     """
     Verify that the form has all the required fields.
 
@@ -80,12 +81,15 @@ def _verifyForm(form: dict, fields: list) -> tuple[bool, str]:
     Returns:
         bool: If the form is valid.
     """
-    # Check if the form has all the required fields
-    for field in fields:
-        if field not in form:
-            return False, f"Missing field: {field}"
+    # Check if the form has all the required fields.
+    logger.debug(f"Form: {form}")
 
-    return True, ""
+    # Count over the fields
+    for number, field in enumerate(fields):
+        if field not in form or form[field] == "":
+            return False, f"Missing field: {field}", number
+
+    return True, "", 0
 
 
 @app.route("/", methods=["GET"])
@@ -145,24 +149,27 @@ def _auth_login() -> str | Response:
 
     # Request is POST
     formFields: list = ["email", "password", "2fa"]
-    formValid, formError = _verifyForm(request.form, formFields)
+    formValid, formError, errorNumber = _verifyForm(request.form, formFields)
 
-    if not formValid:
+    if not formValid and errorNumber != 2:
         return renderTemplate("auth/login.html", error=formError)
 
     # Get the user
     user: User = database.getUserByEmail(request.form["email"])
 
-    # Ensure the password is correct
-    if not user.checkPassword(request.form["password"]):
-        return renderTemplate("auth/login.html", error="Invalid Password")
-
-    # Remove password from memory
-    request.form.pop("password")
-
     # Ensure the user exists
     if user is None:
         return renderTemplate("auth/login.html", error="Invalid Email")
+
+    # Check if the user has 2FA enabled
+    if user.otpKey == "" and errorNumber == 2:
+        session["id"] = user.id
+        session["2fa"] = False
+        return redirect(urlFor("auth._auth_2fa_add"))
+
+    # Ensure the password is correct
+    if not user.checkPassword(request.form["password"]):
+        return renderTemplate("auth/login.html", error="Invalid Password")
 
     # Ensure the user is not banned
     if user.banned:
@@ -217,7 +224,7 @@ def _auth_register() -> str | Response:
 
     # Request is POST
     formFields: list = ["email", "password", "confirm-password", "username"]
-    formValid, formError = _verifyForm(request.form, formFields)
+    formValid, formError, errorNumber = _verifyForm(request.form, formFields)
 
     if not formValid:
         return renderTemplate("auth/register.html", error=formError)
@@ -228,8 +235,6 @@ def _auth_register() -> str | Response:
         del request.form["confirm-password"]
         return renderTemplate("auth/register.html", error="Passwords do not match")
 
-    del request.form["confirm-password"]
-
     # Ensure the email is not already in use
     if database.getUserByEmail(request.form["email"]) is not None:
         return renderTemplate("auth/register.html", error="Email already in use")
@@ -237,7 +242,9 @@ def _auth_register() -> str | Response:
     # Add the user to the database
     database.addUser(request.form["username"], request.form["password"], request.form["email"])
 
-    del request.form["password"]
+    # Sign the user in and redirect to 2fa add
+    user: User = database.getUserByEmail(request.form["email"])
+    session["id"] = user.id
 
     # Redirect to the index
     return redirect(urlFor("auth._auth_2fa_add"))
@@ -278,17 +285,16 @@ def _auth_2fa_add() -> str | Response:
     if request.method == "GET":
         # Create a new TOTP
         key: str = otpKey()
-        url: str = TOTP(key).provisioning_uri(session["email"])
+        url: str = TOTP(key).provisioning_uri(database.getUserByEmail(session["id"]).email, issuer_name="Filehost")
 
         # Set the session
-        session["2fa"] = False
         session["2faKey"] = key
 
         return renderTemplate("auth/2fa_add.html", key=url)
 
     # Request is POST
     formFields: list = ["2fa"]
-    formValid, formError = _verifyForm(request.form, formFields)
+    formValid, formError, errorNumber = _verifyForm(request.form, formFields)
 
     if not formValid:
         return renderTemplate("auth/2fa_add.html", error=formError)
@@ -308,15 +314,15 @@ def _auth_2fa_add() -> str | Response:
     return redirect(urlFor("_index"))
 
 
-@authBlueprint.route("/password/forgot", methods=["GET", "POST"])
-@authBlueprint.route("/password/forgot/", methods=["GET", "POST"])
-def _auth_password_forgot() -> str | Response:
+@authBlueprint.route("/password/reset", methods=["GET", "POST"])
+@authBlueprint.route("/password/reset/", methods=["GET", "POST"])
+def _auth_password_reset() -> str | Response:
     """
-    The password forgot page. This is where the user can reset their password.
+    The password reset page. This is where the user can reset their password.
 
     Returns:
-        Response: The styled password forgot page with the correct messages
-        str: The password forgot page
+        Response: The styled password reset page with the correct messages
+        str: The password reset page
     """
     endpointLogger.logRequest(request)
 
@@ -339,21 +345,21 @@ def _auth_password_forgot() -> str | Response:
 
     # Handle GET first
     if request.method == "GET":
-        return renderTemplate("auth/password_forgot.html")
+        return renderTemplate("auth/password_reset.html")
 
     # Request is POST
     formFields: list = ["email"]
-    formValid, formError = _verifyForm(request.form, formFields)
+    formValid, formError, errorNumber = _verifyForm(request.form, formFields)
 
     if not formValid:
-        return renderTemplate("auth/password_forgot.html", error=formError)
+        return renderTemplate("auth/password_reset.html", error=formError)
 
     # Get the user
     user: User = database.getUserByEmail(request.form["email"])
 
     # Ensure the user exists
     if user is None:
-        return renderTemplate("auth/password_forgot.html", error="Invalid Email")
+        return renderTemplate("auth/password_reset.html", error="Invalid Email")
 
     # Generate a new OTP
     code: str = otpKey()
@@ -361,6 +367,7 @@ def _auth_password_forgot() -> str | Response:
 
     # Send the email
     # emailer.sendVerificationEmail(user.email, code)
+
 
 @authBlueprint.route("/logout", methods=["GET"])
 @authBlueprint.route("/logout/", methods=["GET"])
@@ -378,6 +385,20 @@ def _auth_logout(condition: str | None = None) -> str:
 
     # Redirect to the index
     return renderTemplate("auth/logout.html", condition=condition)
+
+
+@app.route("/test/<string:template>")
+def _test(template: str) -> str:
+    """
+    Test route for testing templates.
+
+    Args:
+        template (str): The template to test.
+
+    Returns:
+        str: The rendered template.
+    """
+    return renderTemplate(f"auth/{template}.html")
 
 
 # Register Blueprints
