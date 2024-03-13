ff75cee8e289c6683f93ef5c64c119ca7f59ac96
CamelGamer123 <kylepannan@gmail.com>
2024-03-13 11:25:28 +1000

    Added upload frontend files. TO BE IMPLEMENTED

---
+++ static/js/upload.js
@@ -0,0 +1,7 @@
+function addElement() {
+    // Get the table element
+    var table = document.getElementById("file-table");
+
+    // Add a new row at the end of the table
+    var newRow = table.insertRow(-1);
+}
