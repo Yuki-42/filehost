9d6eeffaa34c86476953f70190a81577b914e33b
CamelGamer123 <kylepannan@gmail.com>
2024-03-14 11:00:44 +1000

    Added `download()` method to file.js

--- static/js/file.js
+++ static/js/file.js
@@ -1,7 +1,15 @@
 function updateQuality() {
-    const qualitySelector = document.getElementById('quality');
+    const qualitySelector = document.getElementById("quality");
     const selectedQuality = qualitySelector.options[qualitySelector.selectedIndex].value;
 
-    const videoPlayer = document.getElementById('videoPlayer');
-    videoPlayer.src = '' + selectedQuality;  // TODO: Update the video source
+    const videoPlayer = document.getElementById("video");
+    videoPlayer.src = "" + selectedQuality;  // TODO: Update the video source
+}
+
+function download(url) {
+    const iframe = document.createElement("iframe");
+    iframe.style.display = "none";
+
+    iframe.src = url;
+    document.body.appendChild(iframe);
 }
